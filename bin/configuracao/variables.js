bdc7dbf5a6acf5853a0660193a154cb042212d12
Jonas Pequeno <Jonaspdasilva31@gmail.com>
2019-02-26 21:13:15 -0300

    mudança de banco

---
+++ bin/configuracao/variables.js
@@ -0,0 +1,9 @@
+const variables = {
+    Api: {
+        port: 3000
+    },
+    Database: {
+        connection: 'mongodb://admin:admin123@ds153495.mlab.com:53495/heroku_08pn8ntl'
+    }
+}
+module.exports = variables;
